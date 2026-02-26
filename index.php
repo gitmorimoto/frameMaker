d83c06043014215cdd9ae57358a801288adc208d
Takehiko Morimoto <moritake1952@gmail.com>
2026-02-26 17:36:03 +0900

    update

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 require_once('src/class.Frame.php');
 require_once('src/class.inner.php');
 $frameObj = new Frame();
-$inner0Obj = new InnerWithInputBox(0,"inpNumber 0","70%","80%","5%","5%");
+//$inner0Obj = new InnerWithInputBox1(inpNumber,"subTitle","width","height","left","top","fontSize");
+$inner0Obj = new InnerWithInputBox1(0,"inpNumber 0","100%","90%","1%","5%","12px");
+$inner1Obj = new InnerWithInputBox1(0,"inpNumber 1","100%","90%","1%","5%","12px");
+$inner2Obj = new InnerWithInputBox1(0,"inpNumber 2","100%","90%","1%","5%","12px");
 ?>
 <html>
     <head>
@@ -15,6 +18,8 @@ $inner0Obj = new InnerWithInputBox(0,"inpNumber 0","70%","80%","5%","5%");
         <div id="" class = "" style ="width:210mm;height:294mm;border:1px solid white">
         <?php 
             $frameObj->frame(0,"100mm","15mm","white","30mm","50mm",$inner0Obj);
+            $frameObj->frame(1,"100mm","15mm","white","30mm","65mm",$inner1Obj);
+            $frameObj->frame(2,"80mm","15mm","white","100mm","90mm",$inner2Obj);
               
             
         ?>
--- src/class.Inner.php
+++ src/class.Inner.php
@@ -7,7 +7,8 @@ class Inner
     protected $contHeight;
     protected $contLeft;
     protected $contTop;
-    function __construct($inNumber,$content,$contWidth,$contHeight,$contLeft,$contTop)
+    protected $fontSize;
+    function __construct($inNumber,$content,$contWidth,$contHeight,$contLeft,$contTop,$fontSize)
 	{
         $this->inNumber = $inNumber;
 		$this->content = $content;
@@ -15,6 +16,7 @@ class Inner
         $this->contHeight = $contHeight;
         $this->contLeft = $contLeft;
         $this->contTop = $contTop;
+        $this->fontSize = $fontSize;
 
 	}
 
@@ -26,6 +28,7 @@ class Inner
         $contHeight = $this->contHeight;
         $contLeft = $this->contLeft;
         $contTop = $this->contTop;
+        $fontSize = $this->fontSize;
         
         $cont = "<div id='content{$inNumber}' class = '' style='
             width:{$contWidth};
@@ -33,14 +36,15 @@ class Inner
             position:absolute;
             left:{$contLeft};
             top:{$contTop};
-            color:white' >
+            color:white;' >
             {$content}
             </div>";
         return $cont;
     }
 }
 
-class InnerWithInputBox extends Inner
+
+class InnerWithInputBox0 extends Inner
 {
     public function content()
     {
@@ -50,6 +54,7 @@ class InnerWithInputBox extends Inner
         $contHeight = $this->contHeight;
         $contLeft = $this->contLeft;
         $contTop = $this->contTop;
+        $fontSize = $this->fontSize;
         
         $cont = "<div id='content{$inNumber}' class = '' style='
             width:{$contWidth};
@@ -57,14 +62,54 @@ class InnerWithInputBox extends Inner
             position:absolute;
             left:{$contLeft};
             top:{$contTop};
-            color:white' >
+            color:white;font-size:{$fontSize}' >
             {$content}
             <input id='td{$inNumber}'
             class='td' style='
-            widht:70%;
+            width:70%;
             height:80%;
             background:darkgreen;
             color:white'>
+            </div>";
+        return $cont;
+    }
+
+}
+
+class InnerWithInputBox1 extends Inner
+{
+    public function content()
+    {
+        $inNumber = $this->inNumber;
+        $content = $this->content;
+        $contWidth = $this->contWidth;
+        $contHeight = $this->contHeight;
+        $contLeft = $this->contLeft;
+        $contTop = $this->contTop;
+        $fontSize = $this->fontSize;
+        
+        $cont = "<div id='content{$inNumber}' class = '' style='
+            width:{$contWidth};
+            height:{$contHeight};
+            position:absolute;
+            left:{$contLeft};
+            top:{$contTop};
+            color:white;display:flex' >
+                <div id='subTitle{$inNumber}' class='subTitle' style=
+                'width:30%;
+                height:98%;
+                border-right:1px solid white;
+                padding:5px;
+                box-sizing: border-box;
+                font-size:{$fontSize}'>
+                {$content}
+                </div>
+                <input id='td{$inNumber}'
+                class='td' style='
+                width:68%;
+                height:95%;
+                background:darkgreen;
+                color:white'>
             </div>";
         return $cont;
     }
